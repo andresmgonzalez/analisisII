30f7ae1be4af2824579e5788f4ca59f2f01fd844
andresmgonzalez <andresmgonzalez@udea.edu.co>
2017-10-28 19:00:26 -0500

    Clases Controller

---
+++ ProyectoAnalisisII/src/co/com/emelyn/erp/controlador/transacciones/PedidoController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.emelyn.erp.controlador.transacciones;
+
+/**
+ *
+ * @author agonzalez
+ */
+public class PedidoController {
+    
+}
